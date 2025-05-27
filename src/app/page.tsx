e46214298672d4bf7a4daf4fbdacae4c7f19cf9c
SantiagoVillagra <sanmavillagra@gmail.com>
2025-05-27 15:35:56 -0300

    subida dia 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <p>HELLO WORLD</p>
+      <h1>ESTO ES LA PRIMERAA PRUEBA</h1>
     </div>
   );
 }
